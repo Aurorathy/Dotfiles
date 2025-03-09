dda5b233ed78dee89a3503b657a2601001f3b413
Aurorathy <aurorathy.vim@gmail.com>
2025-03-09 16:13:19 +0800

    zsh configuration

--- .config/zsh/prompt.zsh
+++ .config/zsh/prompt.zsh
@@ -1,17 +1,7 @@
-autoload -Uz vcs_info
-precmd_vcs_info() { vcs_info }
-precmd_functions+=( precmd_vcs_info )
-
-zstyle ':vcs_info:git:*' formats '%F{yellow}  %b%f'
-
-setopt PROMPT_SUBST
-
-
-
 # Prompts
 
 prompt_new_line=$'\n\n'
 prompt_end=$'\n'
 
-PROMPT="${prompt_end}%F{cyan}%f%S%F{cyan} Aurorathy%f%s%F{cyan}%f ${vcs_info_msg_0_} %F{blue}%1~%f ${prompt_new_line}%F{cyan}> %f"
+PROMPT="${prompt_end}%F{cyan}%f%S%F{cyan} Aurorathy%f%s%F{cyan}%f %F{blue}%1~%f ${prompt_new_line}%F{cyan}> %f"
 RPROMPT=""
